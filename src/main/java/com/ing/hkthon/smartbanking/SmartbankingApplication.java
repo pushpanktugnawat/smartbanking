c58913a8e79ddb47b8e868785f93765032833512
Pushpank Tugnawat <pushpanktugnawat@gmail.com>
2020-08-07 11:13:02 +0200

    initial commit

---
+++ src/main/java/com/ing/hkthon/smartbanking/SmartbankingApplication.java
@@ -0,0 +1,13 @@
+package com.ing.hkthon.smartbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/hkthon/smartbanking/SmartbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.hkthon.smartbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
